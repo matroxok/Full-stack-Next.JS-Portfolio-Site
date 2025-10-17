b0f1dd28eff9b12cebbcf83b559949624f82c000
matroxok <matrox.biznesyt@gmail.com>
2025-10-18 01:49:56 +0200

    to fix portfolio component

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -1,5 +1,9 @@
 import { Martian_Mono } from 'next/font/google'
 import { EvervaultCard, Icon } from './ui/evervault-card'
+// import { portfolio } from '@/lib/skills'
+
+import Link from 'next/link'
+import Image from 'next/image'
 
 const martianMono = Martian_Mono({
 	subsets: ['latin'],
@@ -18,12 +22,15 @@ export default function Portfolio() {
 						</h3>
 					</div>
 				</div>
-				<div className="border border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
-					<Icon className="absolute h-6 w-6 -top-3 -left-3 text-white " />
-					<Icon className="absolute h-6 w-6 -bottom-3 -left-3 text-white " />
-					<Icon className="absolute h-6 w-6 -top-3 -right-3 text-white " />
-					<Icon className="absolute h-6 w-6 -bottom-3 -right-3 text-white " />
-				</div>
+				{/* {portfolio.map(item => (
+					<div className="border border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+						<Icon className="absolute h-6 w-6 -top-3 -left-3 text-white " />
+						<Icon className="absolute h-6 w-6 -bottom-3 -left-3 text-white " />
+						<Icon className="absolute h-6 w-6 -top-3 -right-3 text-white " />
+						<Icon className="absolute h-6 w-6 -bottom-3 -right-3 text-white " />
+						<p></p>
+					</div>
+				))} */}
 			</div>
 		</div>
 	)
